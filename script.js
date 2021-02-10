180bc006df7a7b49c8d1c6ff8c17b25ad13aa0d0
10Al <813008034@qq.com>
2021-02-10 15:54:17 +0800

    finished project for ca

---
+++ script.js
@@ -0,0 +1,13 @@
+function rotate() {
+    document.getElementById("btn").style.zIndex = "-1";
+    document.getElementById("rotate-pic").style.transform = "translate(-50%, -50%) rotateX(-0.5turn)"
+    setTimeout(fde, 2000);
+}
+function fde() {
+    document.getElementById("rotate-pic").style.visibility = "hidden"
+    document.getElementById("pic").classList.toggle('fde');
+    setTimeout(down, 2000);
+}
+function down() {
+    document.getElementById("pic").style.zIndex = "-1";
+}
